fcd72e93ea921a1db85ba1728b42a69bbbd224d0
Tanvi Bhakta <tanvibhakta@gmail.com>
2017-11-11 16:06:06 +0530

    restore oublished date params to original values

--- blog/models.py
+++ blog/models.py
@@ -9,7 +9,7 @@ class Post(models.Model):
     created_date = models.DateTimeField(
             default=timezone.now)
     published_date = models.DateTimeField(
-            blank=False, null=True)
+            blank=True, null=True)
 
     def publish(self):
         self.published_date = timezone.now()
