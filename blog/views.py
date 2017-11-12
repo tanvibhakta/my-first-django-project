d2dffa9c4c161ad28b7aab7ba3c14253184f47d9
Tanvi Bhakta <tanvibhakta@gmail.com>
2017-11-12 09:47:51 +0530

    use post model, try filtering

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from blog.models import Post
+from django.contrib.auth.models import User
+
 
 def post_list(request):
-    return render(request, 'blog/template.html')
+    current = User.objects.get(username='almighty_author')
+    posts = Post.objects.filter(author=current)
+
+    return render(request, 'blog/template.html', { 'posts' : posts })
